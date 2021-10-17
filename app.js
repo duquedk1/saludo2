79c435fb27beb7eb1624ecb90f26a563b4462915
Daniel <duquedk1@gmail.com>
2021-10-16 21:28:37 -0500

    inicial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/makers/:name', (req, res) => {
+    const {name} = req.params;
+    if(!name || name.length === 0) 
+        res.send(`<h1>Hola desconocido!</h1>`);
+    else {
+        res.send(`<h1>Hola ${name.charAt(0).toUpperCase() + name.slice(1)}!</h1>`);
+    }
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
